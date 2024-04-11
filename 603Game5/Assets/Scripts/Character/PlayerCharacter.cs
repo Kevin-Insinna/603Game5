587093eeef31196de64b48a52dd2c70e7dca72af
npf9700 <70346282+npf9700@users.noreply.github.com>
2024-04-11 10:26:25 -0400

    Basic Player Class and ICharacter Interface
    Set up an interface for player characters and enemies to inherit from and created a basic framework for Player class

---
+++ 603Game5/Assets/Scripts/Character/PlayerCharacter.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour, ICharacter
+{
+    private bool isActiveTurn;
+    private bool isStunned;
+    private int damage;
+    private int health;
+    private int speed;
+    private bool hasFlag;
+
+    //private List<Item> inventory;
+
+    public bool IsActiveTurn { get; set; }
+    public bool IsStunned { get; set; }
+    public int Damage { get; }
+    public int Health { get; set; }
+    public int Speed { get; }
+    public bool HasFlag { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(/*TargetLocation*/)
+    {
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+
+    }
+
+    public void Attack(ICharacter target)
+    {
+
+    }
+
+    public void EndTurn()
+    {
+
+    }
+}
