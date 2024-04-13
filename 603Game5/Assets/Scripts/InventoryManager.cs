da3f6801f34708c953cef491ee52edf254d4421c
Rugved Joshi <joshirugved48@gmail.com>
2024-04-12 22:08:27 -0400

    InventorySystem_Update
    Fixed the errors and added the toggle button

--- 603Game5/Assets/Scripts/InventoryManager.cs
+++ 603Game5/Assets/Scripts/InventoryManager.cs
@@ -11,6 +11,9 @@ public class InventoryManager : MonoBehaviour
     public Transform ItemContent;
     public GameObject InventoryItem;
 
+    public Toggle EnableRemove;
+
+    public InventoryItemController[] InventoryItems;
 
     private void Awake()
     {
@@ -29,16 +32,53 @@ public class InventoryManager : MonoBehaviour
 
     public void ListItems()
     {
+        //Clean content before open.
+        foreach (Transform item in ItemContent)
+        {
+            Destroy(item.gameObject);
+        }
+
         foreach (var item in Items)
         {
             GameObject obj = Instantiate(InventoryItem, ItemContent);
             var itemName = obj.transform.Find("ItemName").GetComponent<Text>();
             var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+            var removeButton = obj.transform.Find("RemoveButton").GetComponent<Button>();
 
             itemName.text = item.itemName;
             itemIcon.sprite = item.icon;
 
+            if (EnableRemove.isOn)
+                removeButton.gameObject.SetActive(true);
+        }
+        SetInventoryItems();
+    }
+
+    public void EnableItemsRemove()
+    {
+        if (EnableRemove.isOn)
+        {
+            foreach (Transform item in ItemContent)
+            {
+                item.Find("RemoveButton").gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            foreach (Transform item in ItemContent)
+            {
+                item.Find("RemoveButton").gameObject.SetActive(false);
+            }
         }
     }
 
+    public void SetInventoryItems()
+    {
+        InventoryItems = ItemContent.GetComponentsInChildren<InventoryItemController>();
+        
+        for (int i = 0; i < Items.Count; i++)
+        {
+            InventoryItems[i].AddItem(Items[i]);
+        }
+    }
 }
